1684ac25792466c96c74f3d31772e8a635e141f5
geographicdesign <joshua.l.hughes.pro@gmail.com>
2019-10-05 13:41:11 -0700

    added time tests

---
+++ code/test/insertTime.py
@@ -0,0 +1,44 @@
+import timeit
+import insertionsort
+import cleanup
+import randomArray
+import testLogger
+
+testName = "Insert"
+logName = "sort_tests.csv"
+
+minVal = 0
+maxVal = 10000
+strtN = 5000
+nIncr = 5000
+tests = 10
+
+
+tstLst = randomArray.lstOfLsts(minVal, maxVal, strtN, nIncr, tests)
+
+results = []
+i = 0
+for tst in tstLst:
+  n = len(tst)
+  strt = timeit.default_timer()
+  print("test: {}\nn: {}".format(i, n))
+  insertionsort.insertionSort( tst)
+
+  end = timeit.default_timer()
+  t = end - strt 
+  print("t: {}\n".format(t) )
+
+  
+  result = [testName, n, t]
+  results.append(result)
+
+  i += 1
+
+testLogger.addLogData(logName, results)
+
+cleanup.pyc()
+
+
+
+
+
---
+++ code/test/mergeTime.py
@@ -0,0 +1,42 @@
+import timeit
+import mergesort
+import cleanup
+import randomArray
+import testLogger
+
+
+testName = "Merge"
+logName = "sort_tests.csv"
+
+minVal = 0
+maxVal = 10000
+strtN = 5000
+nIncr = 5000
+tests = 10
+
+
+tstLst = randomArray.lstOfLsts(minVal, maxVal, strtN, nIncr, tests)
+
+results = []
+i = 0
+for tst in tstLst:
+  n = len(tst)
+  strt = timeit.default_timer()
+  print("test: {}\nn: {}".format(i, n))
+  mergesort.mergeSort( tst, 0, len(tst) -1)
+  end = timeit.default_timer()
+  t = end - strt 
+  print("t: {}\n".format(t) )
+  
+  result = [testName, n, t]
+  results.append(result)
+  
+  i += 1
+
+testLogger.addLogData(logName, results)
+cleanup.pyc()
+
+
+
+
+
